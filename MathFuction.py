bc97628644fa32ef9e4d6103523ccc93e21ef9d9
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2024-08-04 00:06:24 +0530

    Update MathFuction.py

--- MathFuction.py
+++ MathFuction.py
@@ -1,19 +1,16 @@
 import math
 
-
 # c = 2(pi)r
 
 radius = float(input("Enter the radius of a circle : "))
 circumference = 2 * math.pi * radius
 print(f"The circumference is : {round(circumference, 2)}cm")
 
-
 # A = (pi)r^2
 
 radius = float(input("Enter the radius of a circle : "))
 area = math.pi * pow(radius, 2)
 print(f"The area of the circle is : {round(area, 2)}cm^2")
-
 
 # c = sqrt(a^2 + b^2)
 
