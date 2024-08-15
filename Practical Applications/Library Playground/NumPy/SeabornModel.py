9410fd21158a2e357682f9a4b890bab2b4237b2b
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2024-08-15 18:23:17 +0530

    Create SeabornModel.py

---
+++ Practical Applications/Library Playground/NumPy/SeabornModel.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+#
+# sns.distplot([0, 1, 2, 3, 4, 5], hist=False)
+#
+# plt.show()
+
+from numpy import random
+
+# x = random.normal(loc=1, scale=2, size=(2, 3))
+#
+# print(x)
+
+sns.distplot(random.normal(size=1000), hist=False)
+
+plt.show()
