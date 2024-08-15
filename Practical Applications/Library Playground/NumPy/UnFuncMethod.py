e60ed8bfb73fd71bce8ad55ac27c4d9345c92979
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2024-08-15 19:05:23 +0530

    Update UnFuncMethod.py

--- Practical Applications/Library Playground/NumPy/UnFuncMethod.py
+++ Practical Applications/Library Playground/NumPy/UnFuncMethod.py
@@ -19,3 +19,36 @@ y = [2, 4, 6, 8]
 
 z = np.add(x, y)
 print(z)
+
+# frompyfunc Method
+
+import numpy as np
+
+def myadd(x, y):
+    return x + y
+
+myadd = np.frompyfunc(myadd, 2, 1)
+
+print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
+
+# simple arithmetic problems
+
+import numpy as np
+
+
+arr1 = np.array([22, 27, 25, 20, 29, 30])
+arr2 = np.array([10, 11, 12, 13, 14, 15])
+
+result1 = np.add(arr1, arr2)
+result2 = np.subtract(arr1, arr2)
+result3 = np.multiply(arr1, arr2)
+result4 = np.divide(arr1, arr2)
+result5 = np.remainder(arr1, arr2)
+result6 = np.mod(arr1, arr2)
+
+print(f'Addition operation : {result1}')
+print(f'Subtract operation : {result2}')
+print(f'Multiply operation : {result3}')
+print(f'Divition operation : {result4}')
+print(f'Remainder operation : {result5}')
+print(f'modulus operation : {result6}')
